b2e50f8d823c018f5d91a5fff37160e0f8df05e4
balaji <balaji_mnp@yahoo.com>
2020-06-29 19:30:38 +0530

    terraform main

--- main.tf
+++ main.tf
@@ -35,8 +35,8 @@ resource "aws_security_group" "myfirst-sg" {
   }
 
   egress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 0
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
